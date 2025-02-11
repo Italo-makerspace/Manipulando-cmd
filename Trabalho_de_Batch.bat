6f4e087ca90fb854fd97f776a805089be7e03d7c
Italo-makerspace <italooliveiralisboa@gmail.com>
2025-02-11 17:04:58 -0300

    Add files via upload

---
+++ Trabalho_de_Batch.bat
@@ -0,0 +1,34 @@
+mkdir Prototipo1
+mkdir Prototipo2
+mkdir Prototipo3
+mkdir Prototipo4
+Pause
+rmdir Prototipo1
+Pause
+cd Prototipo2
+echo Eu acredito que no futuro a inteligência das máquinas será reconhecida como um fato, e as máquinas serão capazes de fazer quase qualquer coisa que um ser humano possa fazer. >Alan_turing.txt
+cd ..
+cd Prototipo4
+echo Em algum momento no futuro, a distinção entre máquinas e seres humanos pode desaparecer. A computação e a inteligência artificial podem, eventualmente, passar a ser um meio para a realização do mais elevado grau de inteligência. >John_Neumann.txt
+cd ..
+cd Prototipo3
+echo A questão não é se podemos fazer isso, a questão é se vamos fazer isso. >Grace_hopper.txt
+pause
+del Grace_hopper.txt
+pause
+cd ..
+cd Prototipo4
+copy John_Neumann.txt Prototipo2
+pause
+cd ..
+move Prototipo2\Alan_turing.txt Prototipo4\John_Neumann.txt
+pause
+rename Prototipo3 Prototipo_sem_nada
+pause
+cd Prototipo4
+type John_Neumann.txt
+pause
+echo Eu acredito que no futuro a inteligência das máquinas será reconhecida como um fato, e as máquinas serão capazes de fazer quase qualquer coisa que um ser humano possa fazer.
+pause
+rem Muito obrigado pela sua presença!!!
+pause
